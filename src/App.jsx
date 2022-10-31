018854558f45aadea81d5d9a849376ecdf1ed3b2
Ali <ali.ulu43@egmail.com>
2022-11-01 02:16:13 +0300

    Add search input and search movie

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import MovieCard from './components/MovieCard';
 import axios from 'axios';
 import "./App.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,7 +12,7 @@ function App() {
   const API_KEY = process.env.REACT_APP_API_KEY
 
   async function fetchMovies(searchText) {
-    const type = searchText ? `search/movie?query=${searchText}&api_key=${API_KEY}` : `discover/movie?api_key=${API_KEY}`;
+    const type = searchText ? `search/movie?query=${searchText}&api_key=${API_KEY}` : `movie/popular?api_key=${API_KEY}`;
     const response = await axios.get(`${API_URL}${type}`);
     setMovies(response.data.results);
   };
@@ -30,15 +31,29 @@ function App() {
     <div className='app'>
       <header className='header'>
         <h1>Movie App</h1>
-        
       </header>
-      <div className="container">
+      <form className='search' onSubmit={handleSearch}>
+        <input type='text' onChange={e => setSearchText(e.target.value)} placeholder="Search movies..." />
+          <button className='search-button' type='submit'>
+          <SearchIcon fontSize="large" />
+          </button>
+      </form>
+      {
+        movies.length > 0 ? (
+          <div className="container">
         {
           movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))
         }
       </div>
+        ): (
+          <div className='empty'>
+            <h2> Movie is not found </h2>
+          </div>
+        )
+      }
+      
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
 function MovieCard({ movie }) {
-    console.log(movie.title);
-
-
+    console.log(movie);
+    const noImage = "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg";
 
     return (
         <div className='movie-card'>
             <div className='movies'>
-            <div className='movie'>
-                <img key={movie.id} src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt="movieimg" />
-            </div>
-            <div className='movie-info'>
-                <h3 className='movie-title'>{movie.title}</h3>
-            </div>
+                <div className='movie'>
+                    {
+                        movie.poster_path ?
+                            <img key={movie.id} src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt="movieimg" />
+                            :
+                            <img className='empty-image' key={movie.id} src={noImage} alt='imagenotfound' />
+                    }
+                </div>
+                <div className='movie-info'>
+                    <h3 className='movie-title'>{movie.title}</h3>
+                </div>
             </div>
         </div>
     )
