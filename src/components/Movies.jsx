b0d629618ac6404ec6c5c40d7b4f971e4b885a9e
Ali <ali.ulu43@egmail.com>
2022-11-02 19:46:34 +0300

    Add infinite scroll component and edit styles

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,40 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
-import axios from 'axios';
-import YouTube from 'react-youtube';
+import React, { useEffect, useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import YouTube from "react-youtube";
 
-import MovieCard from './MovieCard';
-import ModalMovie from './ModelMovie';
+import MovieCard from "./MovieCard";
+import ModalMovie from "./ModelMovie";
 import "../App.css";
-
+import InfiniteScroll from "react-infinite-scroll-component";
+import { CircularProgress } from "@mui/material";
 
 function Movies() {
-
-
   const [movies, setMovies] = useState([]);
   const [searchText, setSearchText] = useState("");
-  const [selectedMovie, setSelectedMovie] = useState({})
+  const [selectedMovie, setSelectedMovie] = useState({});
   const [trailer, setTrailer] = useState(null);
   const [player, setPlayer] = useState(false);
   const [open, setOpen] = useState(false);
   const [hidden, setHidden] = useState(true);
+  const [page, setPage] = useState(2);
+  const [isNoMore, setIsNoMore] = useState(true);
+  const [visible, setVisible] = useState(16);
+
 
   const API_URL = `https://api.themoviedb.org/3/`;
-  const API_KEY = process.env.REACT_APP_API_KEY
+  const API_KEY = "e887f3f56590f840cc3ee946bca85230";
 
   async function fetchMovies(searchText) {
-    const type = searchText ? `search/movie?query=${searchText}&api_key=${API_KEY}&page=` : `movie/popular?api_key=${API_KEY}&page=2`;
+    const type = searchText
+      ? `search/movie?query=${searchText}&api_key=${API_KEY}&page=1`
+      : `movie/popular?api_key=${API_KEY}&page=1`;
     const response = await axios.get(`${API_URL}${type}`);
-
     setMovies(response.data.results);
-  };
+    return response.data.results;
+  }
+  async function getMovies(searchText) {
+    const type = searchText
+      ? `search/movie?query=${searchText}&api_key=${API_KEY}&page=${page}`
+      : `movie/popular?api_key=${API_KEY}&page=${page}`;
+    const response = await axios.get(`${API_URL}${type}`);
+    return response.data.results;
+  }
 
   async function fetchMovieforYoutube(id) {
-    const response = await axios.get(`${API_URL}movie/${id}?api_key=${API_KEY}&append_to_response=videos`);
+    const response = await axios.get(
+      `${API_URL}movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+    );
     const data = response.data;
     if (data.videos && data.videos.results) {
-      const trailer = data.videos.results.find(video => video.name === "Official Trailer")
-      setTrailer(trailer ? trailer : data.videos.results[0])
+      const trailer = data.videos.results.find(
+        (video) => video.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
     }
     return data;
   }
@@ -42,7 +58,7 @@ function Movies() {
   async function selectMovieId(movie) {
     const data = await fetchMovieforYoutube(movie.id);
     setSelectedMovie(data);
-    setHidden(false)
+    setHidden(false);
     setOpen(true);
   }
 
@@ -53,8 +69,7 @@ function Movies() {
   function handleSearch(e) {
     e.preventDefault();
     fetchMovies(searchText);
-
-  };
+  }
 
   function movieTrailer() {
     return (
@@ -68,9 +83,8 @@ function Movies() {
             controls: 0
           }
         }}
-
       />
-    )
+    );
   }
   function trailerPlayer() {
     setPlayer(true);
@@ -80,46 +94,70 @@ function Movies() {
     setOpen(false);
   }
 
+  async function infitineScroll() {
+    const data = await getMovies();
+    console.log(data);
+
+    setMovies([...movies, ...data]);
+    if (data.length < 1 || data.length < 16) {
+      setIsNoMore(false);
+    }
+    setPage(page + 1);
+    setVisible((prevValue) => prevValue + 16);
+  }
+
   return (
-    <div className='app'>
-      <form className='search' onSubmit={handleSearch}>
-        <input type='text' onChange={e => setSearchText(e.target.value)} placeholder="Search movies..." />
-        <button className='search-button' type='submit'>
+    <div className="app">
+      <form className="search" onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          placeholder="Search movies..."
+        />
+        <button className="search-button" type="submit">
           <SearchIcon fontSize="large" />
         </button>
       </form>
-      {
-        open
-        &&
-        < ModalMovie
+      {open && (
+        <ModalMovie
           hidden={hidden}
           open={open}
           selectMovieId={selectMovieId}
           handleClose={handleClose}
           selectedMovie={selectedMovie}
           player={player}
-          movieTrailer={movieTrailer} 
+          movieTrailer={movieTrailer}
           trailerPlayer={trailerPlayer}
-          />
-      }
-
-      {
-        movies.length > 0 ? (
+        />
+      )}
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={infitineScroll}
+        hasMore={isNoMore}
+        style={{overflowY: 'hidden'}}
+        loader={<CircularProgress className="circular-loader" />}
+        endMessage={
+          <p style={{ textAlign: 'center', flexDirection: 'column'}}>
+            <b>Wow! You have seen it all</b>
+          </p>
+        }
+      >
+        {movies.length > 0 ? (
           <div className="container">
-
-            {
-              movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} selectMovie={selectMovieId} />
-              ))
-            }
+            {movies.slice(0, visible).map((movie) => (
+              <MovieCard
+                key={movie.id}
+                movie={movie}
+                selectMovie={selectMovieId}
+              />
+            ))}
           </div>
         ) : (
-          <div className='empty'>
+          <div className="empty">
             <h2> Movie is not Found</h2>
           </div>
-        )
-      }
-
+        )}
+      </InfiniteScroll>
     </div>
   );
 }
